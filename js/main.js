3b11aef1a77c859c1ef19e946dcc6b1a85ca8171
ccancin0 <ccancino@memphis.edu>
2017-07-01 17:11:15 -0500

    only desired anchors have smooth animation when clicked on

--- js/main.js
+++ js/main.js
@@ -1,9 +1,27 @@
 $(document).ready(function() {
   $(window).scroll(function() {
-    if( $(window).scrollTop() > 70) {
+    if( $(window).scrollTop() > 40) {
       $('.navbar').addClass('navbar-fixed');
     }else {
       $('.navbar').removeClass('navbar-fixed');
     }
+
+    if( $(window).scrollTop() <= 1599) {
+      $('a.active').removeClass('active');
+      $('#vs1').addClass('active');
+    }else if( $(window).scrollTop() >= 1600 && $(window).scrollTop() < 4200) {
+      $('a.active').removeClass('active');
+      $('#order1').addClass('active');
+    }else {
+      $('a.active').removeClass('active');
+      $('#chaos1').addClass('active');
+    }
+  });
+
+  $('a').on('click', function(e){
+    e.preventDefault();
+    $('a.active').removeClass('active');
+    $(this).closest('a').addClass('active');
+    $('html,body').animate({scrollTop:$(this.hash).offset().top}, 800);
   });
 });
